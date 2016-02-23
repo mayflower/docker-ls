54a237923c8ad6eba73b3fef71d4c8411f9a7eab
Christian Speckner <christian.speckner@mayflower.de>
2016-02-23 10:07:52 +0100

    Add optional history parsing.

--- cli/docker-ls/cmd_tag_details.go
+++ cli/docker-ls/cmd_tag_details.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"os"
+	"strings"
 
 	"git.mayflower.de/vaillant-team/docker-ls/cli/docker-ls/response"
 	"git.mayflower.de/vaillant-team/docker-ls/lib"
@@ -21,6 +23,9 @@ func (r *tagDetailsCmd) execute(argv []string) (err error) {
 
 	rawManifest := false
 	r.flags.BoolVar(&rawManifest, "raw-manifest", rawManifest, "output raw manifest")
+
+	parseHistory := false
+	r.flags.BoolVar(&parseHistory, "parse-history", parseHistory, "try to parse history in raw manifest")
 
 	err = r.flags.Parse(argv)
 	if err != nil {
@@ -53,12 +58,62 @@ func (r *tagDetailsCmd) execute(argv []string) (err error) {
 	}
 
 	if rawManifest {
-		err = serializeToStdout(tagDetails.RawManifest(), cfg)
+		manifest := tagDetails.RawManifest()
+
+		if parseHistory {
+			r.parseHistory(manifest)
+		}
+
+		err = serializeToStdout(manifest, cfg)
 	} else {
 		err = serializeToStdout(response.NewTagDetailsL0(tagDetails, true), cfg)
 	}
 
 	return
+}
+
+func (r *tagDetailsCmd) parseHistory(rawManifest interface{}) {
+	var manifest map[string]interface{}
+	var ok bool
+
+	if manifest, ok = rawManifest.(map[string]interface{}); !ok {
+		return
+	}
+
+	var history []interface{}
+
+	if _, ok = manifest["history"]; !ok {
+		return
+	}
+	if history, ok = manifest["history"].([]interface{}); !ok {
+		return
+	}
+
+	for _, rawItem := range history {
+		var item map[string]interface{}
+
+		if item, ok = rawItem.(map[string]interface{}); !ok {
+			continue
+		}
+		if _, ok = item["v1Compatibility"]; !ok {
+			continue
+		}
+
+		var rawHistoryItem string
+
+		if rawHistoryItem, ok = item["v1Compatibility"].(string); !ok {
+			continue
+		}
+
+		decoder := json.NewDecoder(strings.NewReader(rawHistoryItem))
+		var decodedHistoryItem interface{}
+
+		if decoder.Decode(&decodedHistoryItem) != nil {
+			continue
+		}
+
+		item["v1Compatibility"] = decodedHistoryItem
+	}
 }
 
 func newTagDetailsCmd(name string) (cmd *tagDetailsCmd) {
